0bff4ccf61cf2511eef287501271bc3e08de8116
Vimulatus <vasu.mit2501@gmail.com>
2024-06-19 18:30:49 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use cli_tool_book::{run, Config};
+use std::env;
+use std::process;
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
